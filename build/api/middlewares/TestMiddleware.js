aaed96efd14898ac2aa9f6aae786b4793db8bfad
owujib <owujibfavour@gmail.com>
2023-10-26 08:09:12 +0100

    removed cache for builds

--- build/api/middlewares/TestMiddleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MiddleWare {
-    verify(req, res, next) {
-        return next();
-    }
-}
-exports.default = new MiddleWare();
